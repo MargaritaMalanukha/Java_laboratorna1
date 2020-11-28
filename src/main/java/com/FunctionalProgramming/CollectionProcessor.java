e3623a8a4d22a5251169459d6619a7b0a36ab4b4
MargaritaMalanukha <margomalanuha@gmail.com>
2020-11-28 11:03:23 +0300

    Added CollectionProcessor

--- src/main/java/com/FunctionalProgramming/CollectionProcessor.java
+++ src/main/java/com/FunctionalProgramming/CollectionProcessor.java
@@ -1,7 +1,54 @@
 package com.FunctionalProgramming;
 
+import com.Lab.products.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CollectionProcessor {
 
+    public static double findMilkFullPrice(ArrayList<Purchaseable> purchase){
 
+        return purchase
+                .stream()
+                .filter(c -> c instanceof MilkProduct)
+                .mapToDouble(Purchaseable::getPrice).sum();
+    }
+
+    public static Purchaseable findTheMostExpensiveProduct(ArrayList<Purchaseable> purchase) {
+        return Collections
+                .max(purchase, Purchaseable::compare);
+    }
+
+    public static double averageProductPrice(ArrayList<Purchaseable> purchase) {
+        return purchase
+                .stream()
+                .mapToDouble(Purchaseable::getPrice)
+                .average()
+                .orElse(0);
+    }
+
+   /* public static Map<Boolean, ArrayList<Purchaseable>> filter(ArrayList<Purchaseable> purchase, Class<T> cls) {
+        Map<Boolean, ArrayList<Purchaseable>> map = new HashMap<>();
+        map.put(true, purchase
+                .stream()
+                .filter(c -> c instanceof cls));
+    }*/
+
+  //  public static
+
+    public static void main(String [] args) {
+        ArrayList<Purchaseable> purchase = new ArrayList<>();
+        purchase.add(Fish.PIKE);
+        purchase.add(Meat.CHICKEN);
+        purchase.add(Fruit.ORANGE);
+        purchase.add(MilkProduct.CHEESE);
+
+        System.out.println(purchase.get(3).getClass());
+
+        System.out.println(findMilkFullPrice(purchase));
+    }
 
 }
--- src/main/java/com/Lab/products/Purchaseable.java
+++ src/main/java/com/Lab/products/Purchaseable.java
@@ -3,4 +3,7 @@ package com.Lab.products;
 public interface Purchaseable {
     String getTitle();
     double getPrice();
+    default int compare(Purchaseable product) {
+        return Double.compare(this.getPrice(), product.getPrice());
+    }
 }
